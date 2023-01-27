85538dab458837a6ffe86a22096b47ae7fd558c9
Zareel Kalam <110910838+Zareel@users.noreply.github.com>
2023-01-27 10:08:43 +0530

    Update script.js

--- 02/script.js
+++ 02/script.js
@@ -1,26 +1,8 @@
 //todo Fetch all the product name and store in an array
 
-// let arr = [];
-// document
-//   .querySelectorAll(".as-imagegrid-item")
-//   .forEach((item) => arr.push(item.innerText.replace("\nSupport", "")));
-// console.log(arr);
-
-// let newarr = [];
-// let list = document.querySelectorAll(".list");
-// list.forEach((item) => newarr.push(item.innerText.replace("\nSupport", "")));
-// console.log(arr);
+ let arr = [];
+ document.querySelectorAll(".as-imagegrid-item").forEach((item) => arr.push(item.innerText.replace("\nSupport", "")));
+ console.log(arr);
 
 
-<ul>
-<li>one</li>
-<li>two</li>
-<li>three</li>
-<li>four</li>
-<li>five</li>
-</ul>
 
-let arr = [];
-let list = document.querySelectorAll(".list");
-list.forEach((item) => arr.push(item.innerText.replace("\nSupport", "")));
-console.log(arr);
